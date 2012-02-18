0e5597ac21c3d5a04c52b1fa9ec7bc50b86bb2e2
Paul Wittmann <paul@railslove.com>
2012-02-18 16:35:30 +0100

    File list: update (removed external_teachers subfolder)

--- create_file_list.rb
+++ create_file_list.rb
@@ -1,4 +1,5 @@
 require 'json'
 File.open('file_list.json', 'w+') do |file|
-  file.write(Dir['*/**'].delete_if{|e| e=~ /^(courses|events|locations|images|schema)\//}.to_json)
+  file_list = Dir['*/**'].delete_if { |e| e =~ /(^(courses|events|locations|images|schema)\/)|([^\.][^j][^s][^o][^n]$)/ }
+  file.write file_list.to_json
 end
