ca841358fdafeb32f8f2f63babf5495354e0a2fd
Paul Wittmann <paul@railslove.com>
2012-02-27 11:19:14 +0100

    added events to file_list

--- create_file_list.rb
+++ create_file_list.rb
@@ -1,6 +1,6 @@
 require 'json'
 File.open('file_list.json', 'w+') do |file|
-  file_list = Dir['*/**'].delete_if {|file_name| file_name =~ /^(events|images|schema)\//}
+  file_list = Dir['*/**'].delete_if {|file_name| file_name =~ /^(images|schema)\//}
   file_list.delete_if {|file_name| file_name !~ /\.json$/}
-  file.write file_list.to_json
+  file.write file_list.to_json.gsub(',', ",\n")
 end
