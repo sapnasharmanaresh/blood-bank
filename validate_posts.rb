5aee318ab0fba204d66e17635822714cd28c64a9
Salim Hbeiliny <salim@railslove.com>
2012-10-18 14:28:04 +0200

    validate tags syntax

--- validate_posts.rb
+++ validate_posts.rb
@@ -33,8 +33,23 @@ describe 'posts' do
 
           raise "Invalid author: #{post[:person]}" if !File.exists? "people/#{post[:person]}.json"
         
+          if post[:keyword_list] && !post[:keyword_list].empty?
+            post[:keyword_list] = post[:keyword_list].split(",") if post[:keyword_list].kind_of?(String)
+            post[:keyword_list].each(&:strip!).each do |keyword|
+              raise "Invalid keyword: #{keyword}" if !(/^[\w-]*$/ === keyword)
+            end
+          end
+
+          if post[:project_list] && !post[:project_list].empty?
+            post[:project_list] = post[:project_list].split(",") if post[:project_list].kind_of?(String)
+            post[:project_list].each(&:strip!).each do |project|
+              raise "Invalid project: #{project}" if !(/^[\w-]*$/ === project)
+            end
+          end
+
           if post[:person_list] && !post[:person_list].empty?
-            post[:person_list].split(",").each(&:strip!).each do |person_slug|
+            post[:person_list] = post[:person_list].split(",") if post[:person_list].kind_of?(String)
+            post[:person_list].each(&:strip!).each do |person_slug|
               raise "Invalid person: #{person_slug}" if !File.exists? "people/#{person_slug}.json"
             end
           end
@@ -43,7 +58,7 @@ describe 'posts' do
       rescue => e
         assertion = false, e.message + "\nIn: #{file}\nplease fix this!"
       else
-         assertion = true
+        assertion = true
       end
       assert *assertion
     end
